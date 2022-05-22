2138be06983f3a38001249908f4bb304115a8a8a
Shota Kubota <ta2@o2p.jp>
2022-05-22 15:46:41 +0900

    test: modify jest setting to support native ESM

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  transformIgnorePatterns: [
+    'node_modules/(?!(vfile|trough|is-plain-obj|bail|unified|unist-util-visit|unist-util-is|mdast-util-to-string|mdast-util-from-markdown|micromark|parse-entities|character-entities|unist-util-stringify-position))'
+  ]
 }
