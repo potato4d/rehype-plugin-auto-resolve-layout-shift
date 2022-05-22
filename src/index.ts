955869093c4c8d767dc81fc9f82f8862c589b874
Shota Kubota <ta2@o2p.jp>
2022-05-22 15:54:36 +0900

    refactor: remove generic type
    `visit()` first argument is not a `hast.Element`, it is `Node` type.

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ function resolveLayoutShiftPlugin(
 
   async function transformer(htmlAST: Node): Promise<Node> {
     const matches: hast.Element[] = []
-    visit<hast.Element>(htmlAST, 'element', (el: hast.Element) => {
+    visit(htmlAST, 'element', (el: hast.Element) => {
       matches.push(el)
     })
     await Promise.all(matches.map((el) => visitor(el)))
